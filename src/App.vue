8c7c5af719fceb5f4c55c1024a534962053f53bc
Timothy Mostert <resident@hotmail.co.za>
2022-05-15 22:52:18 +0200

    mothers birthday page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-100 h-40 flex flex-col justify-center items-center bg-[#4E1609] sticky top-0 left-0 right-0 m-0">
+    <h1 class="text-white p-4 pb-0 text-xl font-bold font-mono">
+      Happy Birthday Mother Dear!
+    </h1>
+    <h3 class="text-white p-4 text-center">
+      (Yes it's today, and you deserve to be celebrated)
+    </h3>
+  </div>
+  <div class="p-4 bg-slate-200">
+    <img src="@/assets/Balloon-Border.png" alt="" srcset="">
+  </div>
+  <div class="bg-[#CC8899] p-4 text-center text-white font-serif">
+    I've made you a puce website, your favourite colour! Will even spell colour with a 'u', only the Queens english today :D 
+  </div>
+  <div class="p-4 bg-slate-200">
+    <div class="text-center font-semibold font-mono">A small collection of things you love and I love about you!</div>
+    <div class="grid grid-cols-2 gap-4 my-4">
+      <img src="@/assets/time-in-cuba.jpg" alt="" srcset="">
+      <div class="flex justify-center items-center font-sans">Travel: remember how much fun we had in cuba.</div>
+      <div class="flex justify-center items-center font-sans">One delightful fishy</div>
+      <img src="@/assets/fissh.jpg" alt="" srcset="">
+      <img src="@/assets/chips.jpg" alt="" srcset="">
+      <div class="flex justify-center items-center font-sans">
+        Another naughty chips 
+      </div>
+      <div class="flex justify-center items-center font-sans">
+        Going above and beyond for the occasion, and being a wierdo in the process
+      </div>
+      <img src="@/assets/dressing-up.jpg" alt="" srcset="">
+      <img src="@/assets/indoor-plants-png-plant-in-pot-11562849738270ywogsfy.png" alt="" srcset="">
+      <div class="flex justify-center items-center font-sans">A good 'ol house plant, even better if it hits my head..</div>
+      <div class="flex justify-center items-center font-sans">Or better yet to experience nature... that's the real deal.</div>
+      <img src="@/assets/in-nature.jpg" alt="">
+      <img src="@/assets/memes.jpg" alt="">
+      <div class="flex justify-center items-center font-sans">A;ways the connoisseur of dank memes</div>
+      <div class="flex justify-center items-center font-sans">And certainly the most bestest of mothers around!</div>
+      <img src="@/assets/tim-jan.jpg" alt="">
+    </div>
+  </div>
+  <div class="bg-[#CC8899] p-4 text-justify text-white font-serif">
+    My dearest mother, a brief message from your only son.
+    <br><br>
+    I love you greatly, and am so very proud to call you my mother.
+    <br> 
+    You care deeply, and leave those you value with no doubt in their minds that this is true.
+    <br>
+    There are few quite so resiliant and strong, no matter what life throws at you, the underlying current of positivity is never lost, and always appreciated.
+    <br>
+    From coffee to photography to digital art you are beyond talented and creative, I'm always eager to show my friends or even a stranger I've just met. Despite the instagram psychophants I'm still your number one fan :)
+    <br>
+    You are someone I know I could always rely on, or turn to in my times of need, no matter how great the problem you would drop everything and be there for me, I can't explain how much I value that.
+    <br>
+    I never feel afraid to say what I think with you, and know you would always listen and try to understand everything.
+    <br>
+    I can see your growth; in mindset, health and values. Even when I'm a relentless pest you listen openly and reflect honestly, not just taking what I say but filtering through your own authentic worldview.
+    <br>
+    You are courageous, genuine and totally eccentric and for all these things you are always you.
+    <br>
+    Lots and lots and lots of Love
+    <br>
+    Kuku
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
